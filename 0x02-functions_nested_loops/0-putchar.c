cf1c80facb1151bcaf8f31051eaadd12f372bdac
salihu <sah.ibrahim1@gmail.com>
2023-02-22 07:32:11 -0800

    project-0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,20 @@
 #include "main.h"
 /**
- * main - to print putchar
- * written by tkay
- * return: 0
+ * main - print _putchar
+ * written by Sahibrahim
+ * Return: 0
  */
+
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+_putchar('_')
+_putchar('p')
+_putchar('u')
+_putchar('t')
+_putchar('c')
+_putchar('h')
+_putchar('a')
+_putchar('r')
+_putchar('\n')
+return (0)
 }
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,7 @@
-0x02. C - Functions, nested loops
+nested loops and how to use them
+function and how do you use functions
+difference between a declaration and a definition of a functio
+What is a prototype
+Scope of variables
+What are the gcc flags -Wall -Werror -pedantic -Wextra -std=gnu89
+What are header files and how to to use them with #include
